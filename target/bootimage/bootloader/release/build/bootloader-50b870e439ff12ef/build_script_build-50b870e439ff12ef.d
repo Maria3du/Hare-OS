ded82c9e9b1ae4c081bc9a2a5f0d94afdcb91ae3
Maria Edu <maria.martins170409@gmail.com>
2026-09-17 16:29:34 -0300

    Delete target directory

--- target/bootimage/bootloader/release/build/bootloader-50b870e439ff12ef/build_script_build-50b870e439ff12ef.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Cliente\Documents\programação\hare_os\target\bootimage\bootloader\release\build\bootloader-50b870e439ff12ef\build_script_build-50b870e439ff12ef.d: build.rs
-
-C:\Users\Cliente\Documents\programação\hare_os\target\bootimage\bootloader\release\build\bootloader-50b870e439ff12ef\build_script_build-50b870e439ff12ef.exe: build.rs
-
-build.rs:
