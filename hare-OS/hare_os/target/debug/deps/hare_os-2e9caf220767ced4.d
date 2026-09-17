b4364a977a6ae9005b8f3b58e4ed19ad1709d0a0
Maria Edu <maria.martins170409@gmail.com>
2026-09-17 16:28:15 -0300

    Delete hare-OS/hare_os directory

--- hare-OS/hare_os/target/debug/deps/hare_os-2e9caf220767ced4.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Cliente\Documents\programação\hare OS\hare-OS\hare_os\target\debug\deps\hare_os-2e9caf220767ced4.d: src\main.rs
-
-C:\Users\Cliente\Documents\programação\hare OS\hare-OS\hare_os\target\debug\deps\libhare_os-2e9caf220767ced4.rmeta: src\main.rs
-
-src\main.rs:
--- hare-OS/hare_os/target/debug/deps/hare_os.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Cliente\Documents\programação\hare OS\hare-OS\hare_os\target\debug\deps\hare_os.d: src\main.rs
-
-C:\Users\Cliente\Documents\programação\hare OS\hare-OS\hare_os\target\debug\deps\hare_os.exe: src\main.rs
-
-src\main.rs:
